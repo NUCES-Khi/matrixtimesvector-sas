29d41b72ea2755e004df63f505f4025321d4a003
sameed75210 <163000934+sameed75210@users.noreply.github.com>
2024-03-13 01:04:03 +0500

    Created vector function for task 4

--- assign1/mXv_mpi_task_4.c
+++ assign1/mXv_mpi_task_4.c
@@ -13,3 +13,16 @@ double *createMatrix(int rows, int cols)
     }
     return matrix;
 }
+// Function to dynamically allocate a vector and fill it with random values
+double *createVector(int size)
+{
+    double *vector = (double *)malloc(size * sizeof(double));
+    for (int i = 0; i < size; i++)
+    {
+        vector[i] = rand() / (double)RAND_MAX;
+    }
+    return vector;
+}
+
+
+
