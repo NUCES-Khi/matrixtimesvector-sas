53e498db367cbdc0c4afe5a243bc39f22bb711e4
sameed75210 <163000934+sameed75210@users.noreply.github.com>
2024-03-12 17:37:42 +0500

    Update mXv.c

--- assign1/mXv.c
+++ assign1/mXv.c
@@ -1,6 +1,6 @@
 /*
- * Programmer(s) : List all those who contributed in this file
- * Date: 
+ * Programmer(s) : Muhammad Sameed, Ali Raza, Syed Saadullah Hussaini
+ * Date: 3/12/2024
  * Desc: Sequential version of matrix vector multiplication. 
  * Note: you will have to make similar files for other 4 version as explained in assign1.md and a batch file to get running times of all 5 programs.
  */
